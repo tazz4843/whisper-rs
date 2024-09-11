7663f97372339f1df12babf29c10469a0b5fd98e
Stephen M. Coakley <me@stephencoakley.com>
2024-09-11 09:52:47 -0500

    cargo fmt

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 use std::env;
 
 fn main() {
-    println!("cargo:rustc-env=WHISPER_CPP_VERSION={}", env::var("DEP_WHISPER_WHISPER_CPP_VERSION").unwrap());
+    println!(
+        "cargo:rustc-env=WHISPER_CPP_VERSION={}",
+        env::var("DEP_WHISPER_WHISPER_CPP_VERSION").unwrap()
+    );
 }
