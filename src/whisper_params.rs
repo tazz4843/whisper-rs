39cf1f4d4059acb86360ed53d423c751c02e214e
Christian Meter <cmeter@googlemail.com>
2025-01-28 15:23:59 +0100

    Remove deprecated method for suppressing non-speech tokens in FullParams

--- src/whisper_params.rs
+++ src/whisper_params.rs
@@ -299,15 +299,6 @@ impl<'a, 'b> FullParams<'a, 'b> {
         self.fp.suppress_blank = suppress_blank;
     }
 
-    /// Set suppress_non_speech_tokens.
-    /// See <https://github.com/openai/whisper/blob/7858aa9c08d98f75575035ecd6481f462d66ca27/whisper/tokenizer.py#L224-L253>
-    /// for more information.
-    ///
-    /// Defaults to false.
-    pub fn set_suppress_non_speech_tokens(&mut self, suppress_non_speech_tokens: bool) {
-        self.fp.suppress_non_speech_tokens = suppress_non_speech_tokens;
-    }
-
     /// Set initial decoding temperature.
     /// See <https://ai.stackexchange.com/a/32478> for more information.
     ///
